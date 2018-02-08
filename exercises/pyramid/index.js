0785e1521bf935c0c363c6eae8e7c1cd3df2a312
Scott <sdmccoy@gmail.com>
2018-02-08 11:56:11 -0800

    pyramid challenge, 1st solution with nested for loops

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,26 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  
+  //take n-1 and add it to n, this is how many # needed in last row, aka columns
+  const midPoint = Math.floor((2*n -1) / 2);
+  let columns = n + (n - 1);
+  // console.log('col= ', columns);
+  // console.log('n = ',n);
+  for(let row = 0; row < n; row++){
+    let level = '';
+    for(let column = 0; column < 2 * n - 1; column++){
+      if(midPoint - row <= column && midPoint + row >= column){
+        level+='#';
+      } else {
+        level+=' ';
+      }
+    }
+    console.log(level);
+  }
+  
+
+}
 
 module.exports = pyramid;
