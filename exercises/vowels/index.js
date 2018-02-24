3edadfdcf1279fc22bc426771d10ba82105e2d3e
Scott <sdmccoy@gmail.com>
2018-02-24 11:18:32 -0800

    vowel challenge; second solution using regex and match helper

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -8,16 +8,23 @@
 //   vowels('Why?') --> 0
 
 //SOLUTION using for loop and hard code elements in a conditional
-function vowels(str) {
-  //declare variable to return
-  let numOfVowels = 0;
-  //loop through str and use conditional to add
-  for (let element of str.toLowerCase()) {
-    if (element === 'a' || element === 'e' || element === 'i' || element === 'o' || element === 'u') {
-      numOfVowels += 1;
-    }
-  }
-  return numOfVowels;
-}
+// function vowels(str) {
+//   //declare variable to return
+//   let numOfVowels = 0;
+//   let charCheck = ['a', 'e', 'i', 'o', 'u'];
+//   //loop through str and use conditional to add
+//   for (let element of str.toLowerCase()) {
+//     if (charCheck.includes(element)) {
+//       numOfVowels++;
+//     }      
+//   }
+//   return numOfVowels;
+// }
 
+//SOLUTION using regex and match method
+function vowels(str){
+  let result = str.match(/[aeiou]/ig);
+  //if no match, match helper will return null. Use ternary to return 0 if so.
+  return result ? result.length : 0;
+}
 module.exports = vowels;
