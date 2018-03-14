b07c1a7a664df3a9641c9406cbdb27c39910de8f
Scott <sdmccoy@gmail.com>
2018-03-14 11:09:16 -0700

    fibonacci challenge, solution with recursion

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,26 +8,36 @@
 // Example:
 //   fib(4) === 3
 
-//SOLUTION using for loop
-function fib(n) {
-  // start of the series
-  let series = [];
-  // if n is 0 or 1 then return 0 and stop
-  if(n === 0 || n === 1) return n;
+// SOLUTION using for loop
+// BIG 0(n)
+// function fib(n) {
+//   // start of the series
+//   let series = [];
+//   // if n is 0 or 1 then return 0 and stop
+//   if(n === 0 || n === 1) return n;
 
-  // loop
-  for (let i = 0; i <= n; i++) {
-    // push in the 0 and 1 to start
-    if (series.length < 2) {
-      series.push(i);
-    } else {
-      // now there's two entries to add and start the series
-      let entry = series[i - 2] + series[i -1];
-      series.push(entry);
-    }
+//   // loop
+//   for (let i = 0; i <= n; i++) {
+//     // push in the 0 and 1 to start
+//     if (series.length < 2) {
+//       series.push(i);
+//     } else {
+//       // now there's two entries to add and start the series
+//       let entry = series[i - 2] + series[i -1];
+//       series.push(entry);
+//     }
+//   }
+//   // return the result
+//   return series[n];
+// }
+
+// SOLUTION with recursion
+function fib(n) {
+  if (n < 2) {
+    return n;
   }
-  // return the result
-  return series[n];
+  // if n is 5, it will call fib with 4 and 3. 4 will then break down to 3 and 2 fib calls, until you get down to 1 and 0 calls and then you add up the 1s as the solution.
+  return fib(n - 1) + fib(n - 2);
 }
 
 module.exports = fib;
